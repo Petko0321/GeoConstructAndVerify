fae5e3cdc41b0353fb04aa29f095c4007d6d807c
Petko Petkov <petko.petkov2010@gmail.com>
2024-11-24 17:34:50 +0200

    Adding basics and methods

--- GeoConstructAndVerify/basics.py
+++ GeoConstructAndVerify/basics.py
@@ -1,1 +1,103 @@
+from sympy import solve, symbols, Symbol, Eq, init_printing, simplify
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def x(self):
+        return self.x
+
+    def y(self):
+        return self.y
  
+    def lie_on_line(self, line):
+        if str(simplify(line.get_equation([self.x, self.y]))) == 'True':
+            return True
+        return False
+
+
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+        if coincide_points(point1, point2):
+            print("The line is not determined as the two given points coincide.")
+
+    def get_equation(self, variables):
+        x, y = variables[0], variables[1]
+        x1, y1 = self.point1.x, self.point1.y
+        x2, y2 = self.point2.x, self.point2.y
+
+        # Coefficients A, B, C for the line equation Ax + By + C = 0
+        A = y1 - y2
+        B = x2 - x1
+        C = x1 * y2 - x2 * y1
+
+        # Return the equation in the form Ax + By + C = 0
+        return Eq(A * x + B * y + C, 0)
+
+
+class Circle:
+    def __init__(self, center, point_on_circle):
+        self.center = center
+        self.point_on_circle = point_on_circle
+        if coincide_points(center, point_on_circle):
+            print("The circle coincides with its center")
+
+    def get_equation(self, variables):
+        h, k = self.center.x, self.center.y
+        x1, y1 = self.point_on_circle.x, self.point_on_circle.y
+
+        # Calculate the radius using the distance formula
+        r_squared = (x1 - h)**2 + (y1 - k)**2
+
+        # Return the equation of the circle in the form (x-h)^2 + (y-k)^2 = r^2
+        return Eq((variables[0] - h)**2 + (variables[1] - k)**2, r_squared)
+
+#Returns the intersecting points symbolically
+def intersect(line_or_circle1, line_or_circle2, variables):
+    if type(line_or_circle1) is type(line_or_circle2) is Line:
+        result = intersect_two_lines(line_or_circle1, line_or_circle2, variables)
+    elif type(line_or_circle1) is type(line_or_circle2) is Circle:
+        result = intersect_two_circles(line_or_circle1, line_or_circle2, variables)
+    elif type(line_or_circle1) is Line and type(line_or_circle2) is Circle:
+        result = intersect_line_circle(line_or_circle1, line_or_circle2, variables)
+    elif type(line_or_circle1) is Circle and type(line_or_circle2) is Line:
+        result = intersect_line_circle(line_or_circle2, line_or_circle1, variables)
+    else:
+        print("Invalid argument!")
+        result = None
+    return result
+
+def intersect_two_lines(line1, line2, variables):
+    # Solve for the intersection point symbolically
+    x = variables[0]
+    y = variables[1]
+    # variables.remove(x)
+    # variables.remove(y)
+    return [line1.get_equation([x, y]), line2.get_equation([x, y])]
+
+def intersect_two_circles(circle1, circle2, variables):
+# Solve for the intersection point symbolically
+    x = variables[0]
+    y = variables[1]
+    # variables.remove(x)
+    # variables.remove(y)
+    return [circle1.get_equation([x, y]), circle2.get_equation([x, y])]
+
+def intersect_line_circle(line, circle, variables):
+# Solve for the intersection point symbolically
+    x = variables[0]
+    y = variables[1]
+    # variables.remove(x)
+    # variables.remove(y)
+    return [line.get_equation([x, y]), circle.get_equation([x, y])]
+
+# Checks if points eventually coincide
+def coincide_points(point1, point2):
+    if point1.x == point2.x and point1.y == point2.y:
+        return True
+    else:
+        return False
+init_printing()
+x, y = symbols('x y')
--- GeoConstructAndVerify/methods.py
+++ GeoConstructAndVerify/methods.py
@@ -1,1 +1,137 @@
- 
+
+import math
+from sympy import solve, symbols, Symbol, Eq, init_printing, simplify
+from basics import Line, Circle, intersect, Point, x, y
+
+def perpendicular_bisector(point1, point2, variables):
+    # Returns the equations which have their roots the coordinates of the intersecting
+    # points which determine the perpendicular bisector
+    c1 = Circle(point1, point2)
+    c2 = Circle(point2, point1)
+    equations = intersect(c1, c2, variables)
+    print(equations[0])
+    print(equations[1])
+    print(f"--> coordinates ({variables[0]}1,{variables[1]}1), ({variables[0]}2,{variables[1]}2) of the two intersecting points")
+    return equations
+    
+    # If we wanted to return the equation of the perpendicular bisector, we'd used this:
+    # intersecting_points = solve(equations, variables)
+    # l2 = Line(Point(intersecting_points[0][0], intersecting_points[0][1]), Point(intersecting_points[1][0], intersecting_points[1][1]))
+    # return simplify(l2.get_equation([x, y]))
+    # But it's too slow
+    
+
+def perpendicular_line(point, line, point_is_on_line, variables):
+    # Returns the equations used to construct a perpendicular line
+    # Two possible routes depending on if the point lies on the line
+    all_equations = []
+    if point_is_on_line:
+        e = variables[0]
+        f = variables[1]
+        e1 = Symbol(f'{variables[0]}1')
+        e2 = Symbol(f'{variables[0]}2')
+        f1 = Symbol(f'{variables[1]}1')
+        f2 = Symbol(f'{variables[1]}2')
+        c1 = Circle(point, line.point1)
+        equations = intersect(c1, line, [e, f])
+        print(equations[0])
+        all_equations.append(equations[0])
+        print(equations[1])
+        all_equations.append(equations[1])
+        print(f"--> coordinates ({e}1,{f}1), ({e}2,{f}2) of the two intersecting points")
+        variables.remove(e)
+        variables.remove(f)
+        h = variables[0]
+        k = variables[1]
+        equations = perpendicular_bisector(Point(e1, f1), Point(e2, f2), [h, k])
+        all_equations.append(equations[0])
+        all_equations.append(equations[1])
+        print(f"The points ({h}1,{k}1) and ({h}2,{k}2) determine the wanted line")
+        variables.remove(h)
+        variables.remove(k)
+    else:
+        h = variables[0]
+        k = variables[1]
+        c1 = Circle(line.point1, point)
+        c2 = Circle(line.point2, point)
+        equations = intersect(c1, c2, [h, k])
+        print(equations[0])
+        all_equations.append(equations[0])
+        print(equations[1])
+        all_equations.append(equations[1])
+        print(f"--> coordinates ({h}1,{k}1), ({h}2,{k}2) of the two intersecting points")
+        print(f"The points ({h}1,{k}1) and ({h}2,{k}2) determine the wanted line")
+        variables.remove(h)
+        variables.remove(k)
+    return all_equations
+
+def parallel_line(point, line, variables):
+    # Construct a parallel line through point not lying on a given line
+    system = []
+    e1 = Symbol(f'{variables[0]}1')
+    e2 = Symbol(f'{variables[0]}2')
+    f1 = Symbol(f'{variables[1]}1')
+    f2 = Symbol(f'{variables[1]}2')
+    equations = perpendicular_line(point, Line(line.point1, line.point2), False, variables)
+    for eq in equations:
+       system.append(eq)
+    # point_in_on_line is given false
+    l1 = Line(Point(e1, f1), Point(e2, f2))
+    equations = perpendicular_line(p3, l1, True, variables)
+    for eq in equations:
+       system.append(eq)
+    # point_is_on_line is obviously true 
+    # Has to be considered how to automatize this using a function
+    return system
+
+
+class Segment:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+    def length(self):
+        return math.sqrt((self.point1.x - self.point2.x)**2 + (self.point1.y-self.point2.y)**2)
+
+class Vector:
+    def __init__(self, starting_point, ending_point):
+        self.starting_point = starting_point
+        self.ending_point = ending_point
+    def length(self):
+        return math.sqrt((self.starting_point.x - self.ending_point.x)**2 + (self.starting_point.y-self.ending_point.y)**2)
+    
+def Translate(vector, point, variables):
+    # Translate a vector method (not enhanced)
+   p1 = vector.starting_point
+   p2 = vector.ending_point
+   p3 = point
+   line1 = Line(p1, p2)
+   line2 = Line(p1, p3)
+   system = []
+   equations = parallel_line(p3, line1, variables)
+   for eq in equations:
+       system.append(eq)
+   index = len(vars) - len(variables) - 2
+   line3 = Line(Point(Symbol(f'{vars[index]}1'), Symbol(f'{vars[index+1]}1')), Point(Symbol(f'{vars[index]}2'), Symbol(f'{vars[index+1]}2')))
+   print(line3.get_equation([x, y]))
+   equations = parallel_line(p2, line2, variables)
+   for eq in equations:
+       system.append(eq)
+   index = len(vars) - len(variables) - 2
+   line4 = Line(Point(Symbol(f'{vars[index]}1'), Symbol(f'{vars[index+1]}1')), Point(Symbol(f'{vars[index]}2'), Symbol(f'{vars[index+1]}2')))
+   equations = intersect(line3, line4, [x, y])
+   print(equations[0])
+   print(equations[1])
+   print(f"--> coordinates (x, y) of the intersecting point")
+   print("The wanted line segment is from p3(c3, d3) to X(x, y)")
+   for eq in equations:
+       system.append(eq)
+   return system
+# Translate a vector 
+c1, d1, c2, d2, c3, d3 = symbols('c1 d1 c2 d2 c3 d3')
+a, b, e, f, g, h, i, j, k, l, m, n, o, p, v, u, w, z = symbols('a b e f g h i j k l m n o p v u w z')
+all_variables = [a, b, e, f, g, h, i, j, k, l, m, n, o, p, v, u, w, z]
+vars = [a, b, e, f, g, h, i, j, k, l, m, n, o, p, v, u, w, z]
+p1 = Point(c1, d1)
+p2 = Point(c2, d2)
+p3 = Point(c3, d3)
+Translate(Vector(p1,p2),p3, all_variables)
